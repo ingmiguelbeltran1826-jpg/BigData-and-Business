e3271847b8cbaba505fd78c3f6a501f7dd95b022
Miguel Saavedra <migueldjsaavedra@gmail.com>
2026-05-22 10:54:39 -0500

    Add EjerciciosClase1

---
+++ EjerciciosClase1/Ejercicio2.py
@@ -0,0 +1,8 @@
+num1=float((input("Ingrese un número: ")))
+num2=float((input("Ingrese otro número: ")))
+
+def operacion():
+    return ((num1 + num2)/5**2)/2
+
+print("El resultado de la operación es:", operacion())
+
